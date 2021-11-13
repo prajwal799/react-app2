c85473dd22861441baa7da4f122620dbde53d289
prajwal799 <prajwalgoyal1999@gmail.com>
2021-11-13 23:02:54 +0530

    Mobile Card

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <div className="App">
+       <ul>
+         <h2>Mobile Operating System</h2>
+         <li>Android</li>
+         <li>Blackberry</li>
+         <li>iPhone</li>
+         <li>Windows Phone</li>
+       </ul>
+
+       <ul>
+         <h2>Mobile Manufacturers</h2>
+         <li>Samsung</li>
+         <li>HTC</li>
+         <li>Micromax</li>
+         <li>Apple</li>
+       </ul>
+    </div>
+  );
+}
+
+export default App;
